6a4fc464d0cdab2cae59b06212222f2133d013c2
Saba Azeem <sabaazeem025@gmail.com>
2023-10-02 21:26:05 +0500

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DiceGame
-It is made by html,css and javascript
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Random no generate
 let randomNo1=Math.floor(Math.random()*6)+1;
 //pic of dice change according to random no
-let randomDiceImage1='DicePics/dice'+randomNo1+'.png';
+let randomDiceImage1='images/dice'+randomNo1+'.png';
 //get image of player1 from DOM
 let image1=document.querySelectorAll('img')[0];
 //finally change the image1 according to random No
@@ -13,7 +13,7 @@ image1.setAttribute('src',randomDiceImage1);
 //Random no generate
 let randomNo2=Math.floor(Math.random()*6)+1;
 //pic fo dice change according to random no
-let randomDiceImage2='DicePics/dice'+randomNo2+'.png';
+let randomDiceImage2='images/dice'+randomNo2+'.png';
 //get image of player2 from DOM
 let image2=document.querySelectorAll('img')[1];
 //Change the image2 according to the random no
